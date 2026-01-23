15e7906701e80dd024444576b0a1ea082c4000ab
brotherlogic <brotherlogic@gmail.com>
2026-01-23 06:27:56 -0800

    Adds ability to integration tests

--- integration/health_test.go
+++ integration/health_test.go
@@ -2,8 +2,10 @@ package integration
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"testing"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -22,7 +24,7 @@ func TestHealth(t *testing.T) {
 	defer i.teardownContainer(t)
 
 	// Send a basic health check to the server via grpc
-	conn, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient(fmt.Sprintf(":%v", i.mp), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
@@ -31,4 +33,6 @@ func TestHealth(t *testing.T) {
 	if err != nil {
 		t.Errorf("Unable to determine health: %v", err)
 	}
+	time.Sleep(time.Second * 30)
+
 }
--- integration/scaffolding.go
+++ integration/scaffolding.go
@@ -5,10 +5,12 @@ import (
 	"testing"
 
 	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 type integrationTest struct {
-	c testcontainers.Container
+	c  testcontainers.Container
+	mp int
 }
 
 func runTestServer(ctx context.Context) (*integrationTest, error) {
@@ -20,11 +22,20 @@ func runTestServer(ctx context.Context) (*integrationTest, error) {
 				Dockerfile: "Dockerfile",
 			},
 		),
+		testcontainers.WithWaitStrategy(
+			wait.ForListeningPort("8080/tcp"),
+		),
 	}
 	tc, err := testcontainers.Run(ctx, "", opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	mp, err := tc.MappedPort(ctx, "8080/tcp")
 
 	return &integrationTest{
-		c: tc,
+		c:  tc,
+		mp: mp.Int(),
 	}, err
 }
 
