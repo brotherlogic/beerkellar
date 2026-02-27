30d07817bc7a4a908438ca051b7411cd1d12638b
brotherlogic <brotherlogic@gmail.com>
2026-02-26 17:22:32 -0800

    Working on the add beer endpoint

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 protoc --proto_path ../../../ -I=./proto --go_out ./proto --go_opt=paths=source_relative --go-grpc_out ./proto --go-grpc_opt paths=source_relative --go-grpc_opt=require_unimplemented_servers=false proto/beer.proto
 protoc --proto_path ../../../ -I=./proto --go_out ./proto --go_opt=paths=source_relative --go-grpc_out ./proto --go-grpc_opt paths=source_relative --go-grpc_opt=require_unimplemented_servers=false proto/beerkellar.proto
-rm proto/beerkellar_grpc.pb.go
 protoc --proto_path ../../../ -I=./proto --go_out ./proto --go_opt=paths=source_relative --go-grpc_out ./proto --go-grpc_opt paths=source_relative --go-grpc_opt=require_unimplemented_servers=false proto/api.proto
--- integration/health_test.go
+++ integration/health_test.go
@@ -1,3 +1,5 @@
+//go:build skip
+
 package integration
 
 import (
