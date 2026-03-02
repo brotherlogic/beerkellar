31a0fd5f65e4a6fd7d65caef8058a878c1afb141
brotherlogic <brotherlogic@gmail.com>
2026-03-02 23:37:58 +0000

    Working on get cellar

--- beerkellar_cli/cli.go
+++ beerkellar_cli/cli.go
@@ -27,6 +27,14 @@ func main() {
 	defer cancel()
 
 	switch os.Args[1] {
+	case "cellar":
+		cellar, err := client.GetCellar(ctx, &pb.GetCellarRequest{})
+		if err != nil {
+			log.Fatalf("Unable to get cellar: %v", err)
+		}
+		for i, beer := range cellar.GetBeers() {
+			log.Printf("%v. %v - %v", i, beer.GetBrewery(), beer.GetName())
+		}
 	case "login":
 		url, err := client.GetLogin(context.Background(), &pb.GetLoginRequest{})
 		if err != nil {
