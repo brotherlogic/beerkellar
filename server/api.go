a03010f30999ec8ea512cd6ebbde3e0f402a5da5
brotherlogic <brotherlogic@gmail.com>
2026-03-05 14:17:38 +0000

    Also fill out brewery name and beer name

--- server/api.go
+++ server/api.go
@@ -175,7 +175,7 @@ func (s *Server) GetCellar(ctx context.Context, _ *pb.GetCellarRequest) (*pb.Get
 
 			beers = append(beers, &pb.Beer{Id: b.BeerId})
 		} else {
-			beers = append(beers, &pb.Beer{Id: b.BeerId, Abv: beer.GetAbv()})
+			beers = append(beers, &pb.Beer{Id: b.BeerId, Brewery: beer.GetBrewery(), Name: beer.GetName(), Abv: beer.GetAbv()})
 		}
 	}
 
