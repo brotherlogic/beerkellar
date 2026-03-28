20b6bef86b1438f8c2d6ff37593fe0379e2616c1
Simon Tucker <brotherlogic@gmail.com>
2026-03-28 15:25:32 -0700

    Supports the refresh process

---
+++ server/refresh_test.go
@@ -0,0 +1,55 @@
+package server
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	pb "github.com/brotherlogic/beerkellar/proto"
+)
+
+func TestRunRefresh(t *testing.T) {
+	ctx := context.Background()
+	s := getTestServer(ctx)
+
+	// Add a user who needs refresh (LastFeedPull = 0)
+	err := s.db.SaveUser(ctx, &pb.User{
+		Username:     "user1",
+		Auth:         "auth1",
+		LastFeedPull: 0,
+	})
+	if err != nil {
+		t.Fatalf("Unable to save user: %v", err)
+	}
+
+	// Add a user who doesn't need refresh (LastFeedPull = now)
+	now := time.Now().Unix()
+	err = s.db.SaveUser(ctx, &pb.User{
+		Username:     "user2",
+		Auth:         "auth2",
+		LastFeedPull: now,
+	})
+	if err != nil {
+		t.Fatalf("Unable to save user: %v", err)
+	}
+
+	s.runRefresh(ctx)
+
+	// Check if user1 was refreshed (LastFeedPull should be updated)
+	u1, err := s.db.GetUserByName(ctx, "user1")
+	if err != nil {
+		t.Fatalf("Unable to get user1: %v", err)
+	}
+	if u1.GetLastFeedPull() == 0 {
+		t.Errorf("User1 was not refreshed")
+	}
+
+	// Check if user2 was NOT refreshed (LastFeedPull should be the same)
+	u2, err := s.db.GetUserByName(ctx, "user2")
+	if err != nil {
+		t.Fatalf("Unable to get user2: %v", err)
+	}
+	if u2.GetLastFeedPull() != now {
+		t.Errorf("User2 was refreshed when it shouldn't have been: %v vs %v", u2.GetLastFeedPull(), now)
+	}
+}
