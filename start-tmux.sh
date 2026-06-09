9fc7b5d8905484d58d7e4c333c1a9ccff3c06237
Brotherlogic Automation <brotherlogicautomation@gmail.com>
2026-06-08 19:57:23 -0700

    Fixes tmux

--- start-tmux.sh
+++ start-tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure the 'prod' session exists
-if ! tmux has-session -t dcm 2>/dev/null; then
+if ! tmux has-session -t beerkellar 2>/dev/null; then
   # Create a new session named 'prod', detached
   cd /workspaces/beerkellar
   tmux new-session -d -s beerkellar
